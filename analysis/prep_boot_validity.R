e81eabf0b8a7b10d3d4f9c48416015d29ee8ee65
Jason Berger <jason.berger@uni-wuerzburg.de>
2026-09-18 10:25:44 +0200

    changed date

--- analysis/prep_boot_validity.R
+++ analysis/prep_boot_validity.R
@@ -6,7 +6,7 @@ library(dplyr)
 # tail because the missing-count distribution is highly skewed (median 0,
 # but max up to ~983 in n=25, severe conditions).
 
-if (!exists("RESULTS_DIR", inherits = TRUE)) RESULTS_DIR <- "results/results_2026_05_13"
+if (!exists("RESULTS_DIR", inherits = TRUE)) RESULTS_DIR <- "results/results_2026_05_14"
 ERR_DIR <- file.path(RESULTS_DIR, "errors")
 NBOOT <- 1000L
 
--- analysis/prep_jack_validity.R
+++ analysis/prep_jack_validity.R
@@ -1,7 +1,7 @@
 library(dplyr)
 
 
-if (!exists("RESULTS_DIR", inherits = TRUE)) RESULTS_DIR <- "results/results_2026_05_13"
+if (!exists("RESULTS_DIR", inherits = TRUE)) RESULTS_DIR <- "results/results_2026_05_14"
 ERR_DIR <- file.path(RESULTS_DIR, "errors")
 NBOOT <- 1000L
 
